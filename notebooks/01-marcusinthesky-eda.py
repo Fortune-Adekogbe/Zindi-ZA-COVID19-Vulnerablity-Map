9898783376526acd82624cc300ba7170674534c8
marcusinthesky <gwrmar002@myuct.ac.za>
2020-04-05 22:47:39 +0200

    final env

--- notebooks/01-marcusinthesky-eda.py
+++ notebooks/01-marcusinthesky-eda.py
@@ -10,6 +10,7 @@ from sklearn.decomposition import PCA
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.compose import TransformedTargetRegressor, ColumnTransformer
 from sklearn.metrics import get_scorer
+from pyearth import Earth
 
 from za_covid_map.linear_model import TweedieGLM
 from za_covid_map.mca import MCA
@@ -20,20 +21,33 @@ hv.extension('bokeh')
 # %%
 train = context.catalog.load('train_maskedv2')
 test = context.catalog.load('test_maskedv2')
-descriptions = context.catalog.load('variable_descriptions_v2')
+     = context.catalog.load('variable_descriptions_v2')
 sample_submission = context.catalog.load('samplesubmissionv2')
 
 #%%
 train.target_pct_vunerable.hvplot.kde()
 
+
 # %%
 transformer = Pipeline([('poly', PolynomialFeatures()),
                         ('scale', StandardScaler()),
-                        ('pca', PCA(100)),
+                        ('pca', PCA(15)),
                         ('rescale', StandardScaler())])
+
+glm = TweedieGLM(power=0, max_iter=1000)
+mars = Earth()      
 model = Pipeline([('transformer', transformer),
-                  ('model', TweedieGLM(power=0, max_iter=1000))])
-link = Pipeline([('function', FunctionTransformer())])
+                  ('model', mars)])
+
+offset = 1e-9
+def add(y):
+    return (y/100 + offset)
+
+def subtract(y):
+    return ((y) - offset)*100
+
+
+link = Pipeline([('function', FunctionTransformer(add, subtract, validate=True))])
 scorer = get_scorer('neg_root_mean_squared_error')
 
 pipeline = TransformedTargetRegressor(regressor=model, transformer=link)
@@ -41,12 +55,19 @@ pipeline = TransformedTargetRegressor(regressor=model, transformer=link)
 
 
 #%%
-params = {'regressor__model__power': [0, 2, 3],
-          'regressor__model__alpha': [0, 1e-5, 1e-3, 1e-1, 1]}
-search = RandomizedSearchCV(pipeline, params, scoring=scorer)
+glm_params = {'regressor__model__power': [0, 2, 3],
+          'regressor__model__alpha': [1e-3, 1e-1, 1],
+          'regressor__transformer__pca__n_components': [10, 25, 45]}
 
-# %%
-X_train, y_train = train.drop(columns=['target_pct_vunerable', 'ward']), train.target_pct_vunerable.add(1e-9)
+mars_params = {'regressor__model__max_degree': [1, 2],
+          'regressor__model__max_terms': [15, 20],
+          'regressor__transformer__pca__n_components': [10, 25],
+          'regressor__transformer__poly__degree': [1]}
+
+search = RandomizedSearchCV(pipeline, mars_params, scoring=scorer, 
+n_iter = 5, n_jobs=-1, return_train_score=True)
+
+X_train, y_train = train.drop(columns=['target_pct_vunerable', 'ward']), train.target_pct_vunerable
 
 search.fit(X_train, y_train)
 
@@ -59,9 +80,8 @@ X_test = test.loc[:, X_train.columns]
 
 # %%
 # predict and plot
-y_pred = pd.Series(search.predict(X_test), name = y_train.name)
-context.io.save('submissionkde', y_pred.hvplot.kde())
-
+y_pred = pd.Series(search.predict(X_test), name = y_train.name).clip(0, 100)
+submissionkde = y_pred.hvplot.kde()
 
 
 # %%
@@ -70,3 +90,7 @@ submission = sample_submission
 submission.target_pct_vunerable = y_pred
 
 context.io.save('submission', submission)
+
+# %%
+results.sort_values('mean_test_score').tail()
+
